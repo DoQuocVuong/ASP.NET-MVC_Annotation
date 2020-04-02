9eee1e3411e4af7a2c3b48870ba55ed8dde663e6
thanh thao <dothithanhthao24091980@outlook.com>
2020-04-02 17:29:12 +0700

    Add project files.

---
+++ Context/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using DataAnnotation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DataAnnotation.Context
+{
+    public class EmployeeContext: DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DataAnnotation.Startup))]
+namespace DataAnnotation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
